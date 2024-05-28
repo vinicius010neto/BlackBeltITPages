6c1f6551e7d249468048634b483230fb74f61cd6
vinicius010neto <74117559+vinicius010neto@users.noreply.github.com>
2024-05-28 17:51:15 -0300

    htpp fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+      <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"/> 
+      </head>
       <body className={inter.className}>{children}</body>
+
     </html>
   );
 }
